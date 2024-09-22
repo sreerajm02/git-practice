bc869d58e0eca91fe239508d51753bd7f9cbb417
raj <raj@gamil>
2024-09-22 13:59:21 +0530

    aa

--- 06-variables.sh
+++ 06-variables.sh
@@ -2,7 +2,7 @@
 
 FRUTITS=("appLE" "BANANA" "GRAPSE")
 
-echo "1st fruit $FRUTITS[0]"
-echo "2st fruit $FRUTITS[1]"
-echo "3st fruit $FRUTITS[2]"
-echo "1st fruit $FRUTITS[@]"
+echo "1st fruit ${FRUTITS[0]}"
+echo "2st fruit ${FRUTITS[1]}"
+echo "3st fruit ${FRUTITS[2]}"
+echo "1st fruit ${FRUTITS[@]}"
