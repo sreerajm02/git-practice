dd819ab690a8bc43349c058fccbf591669c20cab
raj <raj@gamil>
2024-09-22 20:05:50 +0530

    aa

--- 12-functions.sh
+++ 12-functions.sh
@@ -17,27 +17,27 @@ dnf list installed git
 
 VALIDATE $?
 
-if [ $? -ne 0 ]
-then 
- echo "Git is not installed,going to installl ...."
- dnf install git -y
- else
-  echo "Git is already installed"
-fi
+# if [ $? -ne 0 ]
+# then 
+#  echo "Git is not installed,going to installl ...."
+#  dnf install git -y
+#  else
+#   echo "Git is already installed"
+# fi
 
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
- echo "mysql is not installed,going to installl ...."
- dnf install mysql -y
-  if [ $? -ne 0 ]
- then
- echo "mysql is istallatin unsuccesfulll" 
- exit 1
- else 
- echo "Mysql is installation success"
- fi
- else
-  echo "mysql is already installed"
-fi
+# if [ $? -ne 0 ]
+# then 
+#  echo "mysql is not installed,going to installl ...."
+#  dnf install mysql -y
+#   if [ $? -ne 0 ]
+#  then
+#  echo "mysql is istallatin unsuccesfulll" 
+#  exit 1
+#  else 
+#  echo "Mysql is installation success"
+#  fi
+#  else
+#   echo "mysql is already installed"
+# fi
