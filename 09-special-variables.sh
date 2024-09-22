5a60caf02b9f7d682e2f993314f49cf2a1d62b13
raj <raj@gamil>
2024-09-22 14:41:20 +0530

    aa

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -5,4 +5,5 @@ echo "I want how many variable pssed to the script : $#"
 echo "I want know current script name : $0"
 echo "current Direcotry : $PWD"
 echo "current Home Direcotry : $HOME"
+echo "current process ID : $$"
 
