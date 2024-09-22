e1a9feead29b60d57f3e6c7aeedb63709b947f60
raj <raj@gamil>
2024-09-22 20:03:34 +0530

    aa

---
+++ 12-functions.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run this script with previlage accesss"
+   exit 1
+fi
+
+VALIDATE(){
+echo "fist $1"
+}
+
+dnf list installed git
+
+VALIDATE $?
+
+if [ $? -ne 0 ]
+then 
+ echo "Git is not installed,going to installl ...."
+ dnf install git -y
+ else
+  echo "Git is already installed"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+ echo "mysql is not installed,going to installl ...."
+ dnf install mysql -y
+  if [ $? -ne 0 ]
+ then
+ echo "mysql is istallatin unsuccesfulll" 
+ exit 1
+ else 
+ echo "Mysql is installation success"
+ fi
+ else
+  echo "mysql is already installed"
+fi
