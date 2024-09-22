02ba27f24222792446214a178ee11896cf9a3fe1
raj <raj@gamil>
2024-09-22 10:52:19 +0530

    ad

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "HI I am sreeraj"
-echo "HI i am raj"  
-echo "HI I am kumar"
-echo "HI i am aaa"  
+USER1 : RAMESH
+USR2 : SURESH
+
+echo "$USER1 HI I am $USER2"
+echo "$USER2 HI i am $USER1"  
