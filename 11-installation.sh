d3514b80db256c774cdef6344006bf9384d875e3
raj <raj@gamil>
2024-09-22 15:47:39 +0530

    aa

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,4 +7,14 @@ if [ $USERID -ne 0 ]
 then
    echo "please run this script with previlage accesss"
    exit 1
-fi   
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+ echo "Git is not installed,going to installl ...."
+ dnf install git -y
+ else
+  echo "Git is already installed"
+fi
